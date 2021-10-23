d6d7664bba75702992d65aed150470c25f8c9b69
YousfYousry <yousfzaghlol@gmail.com>
2021-10-23 20:56:51 +0800

    first commit

---
+++ src/com/company/GUI/OldUnusedPages/HomePage.java
@@ -0,0 +1,30 @@
+package com.company.GUI.OldUnusedPages;
+
+import com.company.GUI.SetPage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HomePage extends JPanel {
+
+    public HomePage() {
+//        JTextField textField = new JTextField("Enter Name");
+
+        JPanel ButtonHolder = new JPanel();
+        JButton EditRecords = new JButton("Edit records");
+        EditRecords.addActionListener(e -> {
+        });
+        JButton ViewProducts = new JButton("View records");
+        ViewProducts.addActionListener(e -> {
+            new SetPage("records");
+        });
+
+        ButtonHolder.setLayout(new GridLayout(1, 2));
+        ButtonHolder.add(EditRecords);
+        ButtonHolder.add(ViewProducts);
+
+
+        setLayout(new BorderLayout());
+        add(ButtonHolder, BorderLayout.CENTER);
+    }
+}
